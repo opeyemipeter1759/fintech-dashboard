5aba1b7193f5bc5977a1f21caf563ffa73674290
opeyemipeter1759 <opeyemiadebowale1759@gmail.com>
2023-03-17 02:47:27 +0100

    fix dashboard set up

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -2,15 +2,43 @@ import React from 'react'
 import {Box} from "@mui/material"
 import Header from '../../components/Header'
 
-const Dashboard = () => {
+const Dashboard = () =>
+{
+  
+    
   return (
       <div>
           <Header title="My Card" />
-          <Box display="flex">
-              <div>
-                  
+          <div className='main'>
+              <div className="first">
+                  <div className="card">
+                      card
+                  </div>
+                  <div className="money">
+                      send money
+                  </div>
+                  <div className="transfer">
+                      transfer
+                  </div>
               </div>
-          </Box>
+              <div className="second">
+                  <div className="second-top">
+                      <div className="chart">
+                          money flow
+                      </div>
+                      <div className="contact">
+                          recent contact
+                      </div>
+                  </div>
+                  <div className="recent-transaction">
+                      recent transaction
+                  </div>
+                  <div className="stat">
+                      statistics
+                  </div>
+              </div>
+          </div>
+
     </div>
   )
 }
